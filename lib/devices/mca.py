7fd5a59dd4441973b9ec862348fecfef9a3ba803
(no author) <(no author)@ac7fc886-e22a-4fb8-a9e7-517b44086b78>
2010-05-07 14:07:39 +0000

    v1 of Mca
    git-svn-id: http://millenia.cars.aps.anl.gov/svn/pyepics3@157 ac7fc886-e22a-4fb8-a9e7-517b44086b78

---
+++ lib/devices/mca.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python 
+import epics
+
+class Mca(epics.Device):
+    """ 
+    SynApps Mca Record.   
+    """
+    _fields = ('CALO', 'CALS', 'CALQ', 'TTH' , 'EGU' , 'PRTM', 'PLTM',
+               'PCT', 'PCTL', 'PCTH', 'CHAS', 'DWEL', 'PSCL', 'ERTM', 'ELTM',
+               'ACT' , 'RTIM', 'STIM'.  'STRT', 'STOP', 'ERAS', 'ACQG', 'PROC',
+               'ERST', 'NUSE', 'NMAX', 'VAL')
+    
+    def __init__(self,prefix):
+        if not prefix.endswith('.'): prefix = "%s." % prefix
+        epics.Device.__init__(self,prefix,self._fields)
+
+    def Read(self):
+        attr = 'VAL'
+        return self.get(attr)
+
