3209cc70ef2ee262646fb4525f28bf9235e1cfce
Matt Newville <newville@cars.uchicago.edu>
2010-10-29 17:06:07 -0500

    improved mapper

--- lib/mapper.py
+++ lib/mapper.py
@@ -7,13 +7,14 @@ class mapper(epics.Device):
     """ 
     Fast Map Database
     """
-    attrs = ('Start', 'Abort', 'scanfile', 'info', 'status', 'message',
+    _attrs = ('Start', 'Abort', 'scanfile', 'info', 'status', 'message',
              'filename', 'basedir', 'workdir',
              'nrow', 'maxrow', 'npts', 'TSTAMP','UNIXTS')
     
     def __init__(self,prefix,filename=None):
-        self.prefix = prefix
-        epics.Device.__init__(self, prefix, attrs=self.attrs)
+        self._prefix = prefix
+        epics.Device.__init__(self, self._prefix,
+                              attrs=self._attrs)
         
     def StartScan(self,filename=None,scanfile=None):
         if filename is not None:
@@ -26,11 +27,12 @@ class mapper(epics.Device):
         self.put('Start',1)
 
     def AbortScan(self,filename=None):
-        self.put('Abort',1)
+        self.Abort = 1
 
     def ClearAbort(self):
-        self.put('Abort',0)
-        self.put('Start',0)
+        self.Abort = 0
+        time.sleep(.025)
+        self.Start = 0
         
     def setTime(self):
         "Set Time"
@@ -49,7 +51,7 @@ class mapper(epics.Device):
         self.put('npts', npts)
         
     def setInfo(self,msg):
-        self.put('info',msg)
+        self.put('info',  msg)
         
     def pv_property(attr, as_string=False,wait=False):
         return property(lambda self:     self.get(attr,as_string=as_string),
@@ -60,23 +62,19 @@ class mapper(epics.Device):
     workdir  = pv_property('workdir',  as_string=True)    
     filename = pv_property('filename', as_string=True)
     scanfile = pv_property('scanfile', as_string=True)    
-    message  = pv_property('message',  as_string=True)    
     info     = pv_property('info',     as_string=True)    
-    status   = pv_property('status',   as_string=True)    
-    npts     = pv_property('npts')
-    # nrow     = pv_property('nrow')
-    maxrow   = pv_property('maxrow')
-    tstamp   = pv_property('TSTAMP')
-    unixts   = pv_property('UNIXTS')
-    start    = pv_property('Start')
-    abort    = pv_property('Abort')
-    
-
-
+    message  = pv_property('message',  as_string=True)    
     
 if __name__ == '__main__':
 
     m = mapper('13XRM:map:')
+ 
     
-    print m.dir
+    print m
+    print m.basedir
+    print m.info
+    print m.TSTAMP
+    print 'unix ts: ', m.UNIXTS
     
+    print m.get('basedir', as_string=True)
+
