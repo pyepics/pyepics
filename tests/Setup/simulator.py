6bd95e68a98e006865527d4979d75f8ca344edff
Matt Newville <newville@cars.uchicago.edu>
2011-03-18 12:07:49 -0500

    start of automated testbed

---
+++ tests/Setup/simulator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+# test simulator for testing pyepics.
+# 
+# this script changes values definied in the pydebug.db, which all 
+# the tests scripts use.  This script must be running (somewhere)
+# for many of the tests (callbacks, etc) to work. 
+
+# 
+import epics
+
+
+
