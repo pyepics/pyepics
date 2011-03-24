5c76634823dc718da45f7cf8e8a13e40db030f9e
Matt Newville <newville@cars.uchicago.edu>
2011-03-24 12:23:26 -0500

    wx: remove setting of default font -- left to Application

--- lib/wx/wxlib.py
+++ lib/wx/wxlib.py
@@ -432,8 +432,8 @@ class pvCtrlMixin(pvMixin):
         self.fgColourAlarms = {}
         self.bgColourAlarms = {}
 
-        if font is None:
-            font = wx.Font(12, wx.SWISS, wx.NORMAL, wx.BOLD,False)
+        #if font is None:
+        #    font = wx.Font(12, wx.SWISS, wx.NORMAL, wx.BOLD,False)
         
         try:
             if font is not None:  self.SetFont(font)
