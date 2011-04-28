fae04308467e6a688efabf89263a6494c4e314ee
Matt Newville <newville@cars.uchicago.edu>
2011-04-28 16:06:25 -0500

    changes to publish and upload scripts

---
+++ publish_docs.sh
@@ -0,0 +1,59 @@
+installdir='/www/apache/htdocs/software/python/pyepics3'
+docbuild='doc/_build'
+
+cd doc 
+echo '# Making docs'
+
+make all
+cd ../
+echo '# Building tarball of docs'
+mkdir _tmpdoc
+cp -pr doc/pyepics.pdf     _tmpdoc/pyepics.pdf
+cp -pr doc/_build/html/*    _tmpdoc/.
+cd _tmpdoc
+tar czf ../../pyepics_docs.tar.gz .
+cd ..
+rm -rf _tmpdoc 
+
+echo "# Switching to gh-pages branch"
+git checkout gh-pages
+
+if  [ $? -ne 0 ]  ; then 
+  echo ' failed.'
+  exit 
+fi
+
+tar xzf ../pyepics_docs.tar.gz .
+
+echo "# commit changes to gh-pages branch"
+git commit -am "changed docs"
+
+if  [ $? -ne 0 ]  ; then 
+  echo ' failed.'
+  exit 
+fi
+
+echo "# Pushing docs to github"
+git push
+
+
+echo "# switch back to master branch"
+git checkout master
+
+if  [ $? -ne 0 ]  ; then 
+  echo ' failed.'
+  exit 
+fi
+# 
+# install locally
+echo "# Installing docs to CARS web pages"
+cp ../pyepics_docs.tar.gz $installdir/../.
+
+cd $installdir
+if  [ $? -ne 0 ]  ; then 
+  echo ' failed.'
+  exit 
+fi
+
+tar xvf ../pyepics_docs.tar.gz .
+
