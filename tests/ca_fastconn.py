00013e866cf3649d410f55ba00373eca2435d143
Matt Newville <newville@cars.uchicago.edu>
2011-11-04 13:56:48 -0500

    test ca_fastconn updated to use ca.get_cached_value()

--- tests/ca_fastconn.py
+++ tests/ca_fastconn.py
@@ -6,41 +6,45 @@ dt = debugtime.debugtime()
 dt.add('test of fast connection to many PVs')
 pvnames = []
 values = []
-pvlist = [] 
+pvlist = []
 data   = {}
 
+MAX_PVS = 10000
 for line  in open('fastconn_pvlist.txt','r').readlines():
     pvnames.append(line[:-1])
-    
+
+if MAX_PVS is not None:
+    pvnames = pvnames[:MAX_PVS]
+
 dt.add('Read PV list:  Will connect to %i PVs' % len(pvnames))
 
 libca = ca.initialize_libca()
 
 for name in pvnames:
     chid = ca.create_channel(name, auto_cb=False)
-
     pvlist.append(chid)
+
 
 dt.add("created PVs with ca_create_channel")
 
-ca.pend_event(1.e-3)
+ca.pend_event(1.e-2)
 ca.pend_io(1.0)
 
 for chid in pvlist:
     name  = ca.name(chid)
     count = ca.element_count(chid)
     ftype = ca.field_type(chid)
-    pdat = ca.get(chid, unpack=False)
-    data[name] = ftype, count,  pdat
-   
+    ca.get(chid, unpack=False)
+    data[name] = chid, ftype, count
+
 dt.add("did ca.get() for value references")
-ca.pend_event(1.e-3)
-ca.pend_io(3.0)
+ca.pend_event(1.e-2)
+
 dt.add("pend complete")
 for name in pvnames:
-    ftype, count, pdat = data[name]
-    val = ca._unpack(pdat, count=count, ftype=ftype)
-    values.append(val) 
+    chid, ftype, count = data[name]
+    val = ca.get_cached_value(chid, count=count, ftype=ftype)
+    values.append(val)
 dt.add("unpacked PV values")
 
 f = open('fastconn_pvdata.sav', 'w')
