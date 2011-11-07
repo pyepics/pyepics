20c003ed0424578debd7571dabed30c7538752b4
Matt Newville <newville@cars.uchicago.edu>
2011-11-07 12:37:29 -0600

    ca_fastconn test using connect=False, explicit connect_channel()

--- tests/ca_fastconn.py
+++ tests/ca_fastconn.py
@@ -1,59 +1,66 @@
 from epics import ca, dbr
 import time
 import debugtime
-
+from epics.wx.ordereddict import OrderedDict
 dt = debugtime.debugtime()
 dt.add('test of fast connection to many PVs')
 pvnames = []
-values = []
-pvlist = []
-data   = {}
+
+results = OrderedDict()
 
 MAX_PVS = 20000
+
 for line  in open('fastconn_pvlist.txt','r').readlines():
     pvnames.append(line[:-1])
 
 if MAX_PVS is not None:
     pvnames = pvnames[:MAX_PVS]
 
-dt.add('Read PV list:  Will connect to %i PVs' % len(pvnames))
 
+dt.add('Read PV list:  Will connect to %i PVs' % len(pvnames))
 libca = ca.initialize_libca()
 
 for name in pvnames:
-    chid = ca.create_channel(name, auto_cb=False)
-    pvlist.append(chid)
+    chid = ca.create_channel(name,  connect=False)
+    results[name] = {'chid': chid}
 
+time.sleep(0.001)
 
 dt.add("created PVs with ca_create_channel")
 
-ca.pend_event(1.e-2)
-ca.pend_io(1.0)
-
-for chid in pvlist:
-    name  = ca.name(chid)
-    count = ca.element_count(chid)
-    ftype = ca.field_type(chid)
-    ca.get(chid, wait=False)
-    data[name] = chid, ftype, count
-
-dt.add("did ca.get() for value references")
-ca.pend_event(1.e-2)
-
-dt.add("pend complete")
 for name in pvnames:
-    chid, ftype, count = data[name]
-    val = ca.get_complete(chid, count=count, ftype=ftype)
-    values.append(val)
-dt.add("unpacked PV values")
+    ca.connect_channel(results[name]['chid'])
 
+time.sleep(0.001)
+
+dt.add("connected to PVs with connect_channel")
+
+ca.pend_event(1.e-2)
+
+for name in pvnames:
+    chid = results[name]['chid']
+    val = ca.get(chid, wait=False)
+    results[name]['value'] =  val
+
+dt.add("did ca.get(wait=False)")
+ca.pend_event(1.e-2)
+dt.add("pend complete")
+
+for name in pvnames:
+    chid = results[name]['chid']    
+    val = ca.get_complete(chid)
+    results[name]['value'] =  val
+    
+
+dt.add("unpacked PV values")
+ 
 f = open('fastconn_pvdata.sav', 'w')
-for n,v in zip(pvnames, values):
-    f.write("%s %s\n" % (n.strip(), v))
+for name, val in results.items():
+    f.write("%s %s\n" % (name.strip(), val['value']))
 f.close()
 dt.add("wrote values PVs")
 
 dt.show()
 
-time.sleep(0.1)
+time.sleep(0.01)
 ca.poll()
