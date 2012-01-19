8c36a7d2937a9c941005680838ecc54772981683
Matt Newville <newville@cars.uchicago.edu>
2012-01-19 16:35:53 -0600

    pv back to auto_monitor=None

--- lib/pv.py
+++ lib/pv.py
@@ -51,7 +51,7 @@ class PV(object):
                'upper_warning_limit', 'upper_ctrl_limit', 'lower_ctrl_limit')
 
     def __init__(self, pvname, callback=None, form='native',
-                 verbose=False, auto_monitor=True,
+                 verbose=False, auto_monitor=None, 
                  connection_callback=None,
                  connection_timeout=None):
 
