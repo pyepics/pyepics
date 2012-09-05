ad6ceb6bcddfc5deeb9e6d4a539155aff612a388
Matt Newville <newville@cars.uchicago.edu>
2012-09-05 16:24:30 -0500

    move to setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
+
 import os
 import sys
 import lib
 import shutil
+
+long_desc = '''Python Interface to the Epics Channel Access protocol
+of the Epics control system. '''
+
 #
 no_libca="""*******************************************************
 *** WARNING - WARNING - WARNING - WARNING - WARNING ***
@@ -34,18 +40,28 @@ if os.name == 'nt':
 
 PY_MAJOR, PY_MINOR = sys.version_info[:2]
 if PY_MAJOR == 2 and PY_MINOR < 6:
-    shutil.copy(os.path.join('lib', 'utils3.py'), os.path.join('lib', 'utils3_save_py.txt'))
-    shutil.copy(os.path.join('lib', 'utils2.py'), os.path.join('lib', 'utils3.py'))
+    shutil.copy(os.path.join('lib', 'utils3.py'),
+                os.path.join('lib', 'utils3_save_py.txt'))
+    shutil.copy(os.path.join('lib', 'utils2.py'),
+                os.path.join('lib', 'utils3.py'))
 
 setup(name = 'pyepics',
-      version = lib.__version__,
-      author = 'Matthew Newville',
+      version      = lib.__version__,
+      author       = 'Matthew Newville',
       author_email = 'newville@cars.uchicago.edu',
-      url  = 'http://pyepics.github.com/pyepics/',
-      license = 'Epics Open License',
-      description = "Epics Channel Access Extensions to Python",
+      url          = 'http://pyepics.github.com/pyepics/',
+      download_url = 'http://pyepics.github.com/pyepics/',      
+      license      = 'Epics Open License',
+      description  = "Epics Channel Access for Python",
+      long_description = long_desc,
+      platforms    = ('Windows', 'Linux', 'Mac OS X'),
+      classifiers  = ['Intended Audience :: Science/Research',
+                      'Operating System :: OS Independent',
+                      'Programming Language :: Python',
+                      'Topic :: Scientific/Engineering'],      
       package_dir = {'epics': 'lib'},
-      packages = ['epics','epics.wx','epics.devices', 'epics.compat', 'epics.autosave'],
+      packages = ['epics','epics.wx','epics.devices',
+                  'epics.compat', 'epics.autosave'],
       data_files = data_files )
 
 try:
