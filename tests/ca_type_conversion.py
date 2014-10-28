2393cb238c97d2e05d68904c3d8ed0f83c72e476
Matt Newville <newville@cars.uchicago.edu>
2014-10-28 11:15:26 -0500

    test using print function instead of write()

--- tests/ca_type_conversion.py
+++ tests/ca_type_conversion.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import sys
 import time
 import epics
@@ -15,12 +17,11 @@ pvlist = (
     pvnames.double_arr_pv,
     )
 
-write = sys.stdout.write
 
 def RunTest(pvlist, use_preempt=True, maxlen=16384, 
             use_numpy=True, use_time=False, use_ctrl=False):
-    msg= ">>>Run Test: %i pvs, numpy=%s, time=%s, ctrl=%s, preempt=%s\n"
-    write( msg % (len(pvlist), use_numpy, use_time, use_ctrl, use_preempt))
+    msg= ">>>Run Test: %i pvs, numpy=%s, time=%s, ctrl=%s, preempt=%s"
+    print( msg % (len(pvlist), use_numpy, use_time, use_ctrl, use_preempt))
 
     epics.ca.HAS_NUMPY =  epics.ca.HAS_NUMPY and use_numpy
     epics.ca.PREEMPTIVE_CALLBACK = use_preempt
@@ -29,10 +30,10 @@ def RunTest(pvlist, use_preempt=True, maxlen=16384,
     epics.ca.initialize_libca()    
 
     def onConnect(pvname=None,  **kw):
-        write(' on Connect %s %s\n' % (pvname, repr(kw)))
+        print(' on Connect %s %s' % (pvname, repr(kw)))
         
     def onChanges(chid=None, value=None, **kw):
-        write(' on Change chid=%i value=%s\n' % (int(chid), repr(value)))
+        print(' on Change chid=%i value=%s' % (int(chid), repr(value)))
         
     for pvname in pvlist:
         chid = epics.ca.create_channel(pvname, callback=onConnect)
@@ -43,28 +44,28 @@ def RunTest(pvlist, use_preempt=True, maxlen=16384,
     epics.poll(evt=0.025, iot=10.0)
     time.sleep(0.05)
     for (chid, eventID) in chids:
-        write('=== %s   chid=%s\n' % (epics.ca.name(chid), repr(chid)))
+        print('=== %s   chid=%s' % (epics.ca.name(chid), repr(chid)))
         time.sleep(0.005)
         ntype = epics.ca.promote_type(chid, use_ctrl=use_ctrl,
                                       use_time=use_time)
         val  = epics.ca.get(chid, ftype=ntype)
-        time.sleep(0.002)
         cval = epics.ca.get(chid, as_string=True)    
         if epics.ca.element_count(chid) > 10:
             val = val[:10]
-        write("%i %s  %s \n" % (ntype, epics.dbr.Name(ntype).lower(), cval))
+        time.sleep(0.005)
+        print("%i %s %s" % (ntype, epics.dbr.Name(ntype).lower(), cval))
     time.sleep(0.5)        
-    write('----- finalizing CA\n')
+    print('----- finalizing CA')
     epics.ca.finalize_libca()
     time.sleep(0.05)
     
 for use_preempt in (True, False):
-    for use_numpy in (True, False): #  False):
+    for use_numpy in (True, False): 
         for use_time, use_ctrl in ((False, False),
                                    (True, False),
                                    (False, True),
                                    ):
-            print("====\n====  NUMPY/TIME/CTRL ", use_numpy, use_time, use_ctrl)
+            print("====  NUMPY/TIME/CTRL ", use_numpy, use_time, use_ctrl)
             RunTest(pvlist,
                     use_preempt=use_preempt,
                     use_numpy=use_numpy,
