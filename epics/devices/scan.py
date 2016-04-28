50bbc76c673c048da09ef94f3f7065fd87937d21
Matt Newville <newville@cars.uchicago.edu>
2016-04-28 10:58:56 -0500

    more features, fixes to scan device for adding positioners, triggers, detectors

--- epics/devices/scan.py
+++ epics/devices/scan.py
@@ -2,7 +2,7 @@
 """
 Epics scan record
 """
-from .. import Device
+from .. import Device, poll
 import threading
 
 NUM_POSITIONERS = 4
@@ -86,6 +86,7 @@ class Scan(Device):
             self.clear_positioner(i)
         for i in range(1, NUM_DETECTORS+1):
             self.clear_detector(i)
+        poll(1.e-3, 1.0)
 
     def _print(self):
         print('PV = %s' % self.get('P1PV'))
@@ -103,6 +104,7 @@ class Scan(Device):
           idet    index of detector (1 through 70, default 1)
         """
         self.put("D%2.2iPV" % idet, '')
+        poll(1.e-3, 1.0)
 
     def add_detector(self, detector):
         """add a detector to a scan definition
@@ -110,17 +112,21 @@ class Scan(Device):
         Arguments
         ---------
           detector   name of detector pv
+
+        Returns
+        -------
+         idet  index of detector set
         """
         idet = None
         for _idet in range(1, NUM_DETECTORS+1):
+            poll(1.e-3, 1.0)
             if len(self.get('D%2.2iPV' % _idet)) < 2:
                 idet = _idet
                 break
         if idet is None:
             raise ScanException("%i Detectors already defined." % (NUM_DETECTORS))
-        self.put("D%2.2iPV" % idet, detector)
-
-
+        self.put("D%2.2iPV" % idet, detector, wait=True)
+        return idet
 
     def clear_trigger(self, itrig=1):
         """completely clear a trigger
@@ -130,6 +136,7 @@ class Scan(Device):
           itrig    index of trigger (1 through 4, default 1)
         """
         self.put("T%iPV" % itrig, '')
+        poll(1.e-3, 1.0)
 
     def add_trigger(self, trigger, value=1.0):
         """add a trigger to a scan definition
@@ -138,17 +145,23 @@ class Scan(Device):
         ---------
           trigger   name of trigger pv
           value     value to send to trigger (default 1.0)
+
+        Returns
+        -------
+           itrig  index of trigger set
         """
         itrig = None
         for _itrig in range(1, NUM_TRIGGERS+1):
+            poll(1.e-3, 1.0)
             if len(self.get('T%iPV' % _itrig)) < 2:
                 itrig = _itrig
                 break
         if itrig is None:
             raise ScanException("%i Triggers already defined." % (NUM_TRIGGERS))
 
-        self.put("T%iPV" % itrig, trigger)
-        self.put("T%iCD" % itrig, value)
+        self.put("T%iPV" % itrig, trigger, wait=True)
+        self.put("T%iCD" % itrig, value, wait=True)
+        return itrig
 
 
     def clear_positioner(self, ipos=1):
@@ -164,6 +177,7 @@ class Scan(Device):
             if attr == 'PA': nulval = [0]
             self.put("P%i%s" % (ipos, attr), nulval)
         self.put("R%iPV" % ipos, '')
+        poll(1.e-3, 1.0)
 
     def add_positioner(self, drive, readback=None,
                        start=None, stop=None, step=None,
@@ -183,16 +197,22 @@ class Scan(Device):
          center    center value
          width     width value
          array     array of values for table or fly mode
+
+        Returns
+        -------
+         ipos  index of positioner set
+
         """
         ipos = None
         for _ipos in range(1, NUM_POSITIONERS+1):
+            poll(1.e-3, 1.0)
             if len(self.get('P%iPV' % _ipos)) < 2:
                 ipos = _ipos
                 break
         if ipos is None:
             raise ScanException("%i Positioners already defined." % (NUM_POSITIONERS))
 
-        self.put('P%iPV' % ipos, drive)
+        self.put('P%iPV' % ipos, drive, wait=True)
         if readback is None and drive.endswith('.VAL'):
             readback = drive[:-4] + '.RBV'
         if readback is not None:
@@ -227,19 +247,91 @@ class Scan(Device):
         # table or fly mode
         if smode in (1, 2) and array is not None:
             self.put('P%iPA' % ipos, array)
+        poll(1.e-3, 1.0)
+        return ipos
+
+    def set_positioner(self, ipos, drive=None, readback=None,
+                       start=None, stop=None, step=None,
+                       center=None, width=None,
+                       mode=None, absolute=None, array=None):
+        """change a positioner setting in a scan definition
+        all settings are optional, and will leave other settings unchanged
+
+        Arguments
+        ----------
+         drive     name of drive pv
+         readback  name of readback pv
+         mode      positioner mode ('linear', 'table', fly', default 'linear')
+         absolute  whether to use absolute values (True/False, default True)
+         start     start value
+         stop      stop value
+         step      step value
+         center    center value
+         width     width value
+         array     array of values for table or fly mode
+
+        Notes
+        -----
+         This allows changing a scan, for example:
+
+             s = Scan('XXX:scan1')
+             ipos1 = s.add_positioner('XXX:m1.VAL', start=-1, stop=1, step=0.1)
+             ....
+
+             s.run()
+
+         Then changing the scan definition with
+
+            s.set_positioner(ipos1, start=0, stop=0.2, step=0.01)
+            s.run()
+        """
+        if ipos is None:
+            raise ScanException("must give positioner index")
+
+        if drive is not None:
+            self.put('P%iPV' % ipos, drive)
+        if readback is not None:
+            self.put('R%iPV' % ipos, readback)
+        if start is not None:
+            self.put('P%iSP' % ipos, start)
+        if stop is not None:
+            self.put('P%iEP' % ipos, stop)
+        if step is not None:
+            self.put('P%iSI' % ipos, step)
+        if center is not None:
+            self.put('P%iCP' % ipos, center)
+        if width is not None:
+            self.put('P%iWD' % ipos, width)
+        if array is not None:
+            self.put('P%iPA' % ipos, array)
+
+        if absolute is not None:
+            if absolute:
+                self.put('P%iAR' % ipos, 0)
+            else:
+                self.put('P%iAR' % ipos, 1)
+
+        if mode is not None:
+            smode = 0
+            if mode.lower().startswith('table'):
+                smode = 1
+            elif mode.lower().startswith('fly'):
+                smode = 2
+            self.put('P%iSM' % ipos, smode)
+        poll(1.e-3, 1.0)
+
 
     def after_scan(self, mode):
         """set after scan mode"""
-        self.put("PASM", mode)
+        self.put("PASM", mode, wait=True)
 
     def positioner_delay(self, pdelay):
         """set positioner delay in seconds"""
-        self.put("PDLY", pdelay)
+        self.put("PDLY", pdelay, wait=True)
 
     def detector_delay(self, pdelay):
         """set detector delay in seconds"""
-        self.put("DDLY", pdelay)
-
+        self.put("DDLY", pdelay, wait=True)
 
 class ScanException(Exception):
     """ raised to indicate a problem with a scan"""
