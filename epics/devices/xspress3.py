b82b1262f7bceaeaaddacf2a07775f77174d2733
Matthew Newville <newville@cars.uchicago.edu>
2016-05-07 22:04:32 -0500

    refactor xspress3 plugin to support 2 versions

--- epics/devices/xspress3.py
+++ epics/devices/xspress3.py
@@ -6,19 +6,187 @@ from ConfigParser import ConfigParser
 
 from epics import Device, caget, caput, poll
 from epics.devices.mca import MCA, ROI, OrderedDict
-    
+from epics.devices.ad_mca import ADMCA, ADMCAROI
+
 MAX_ROIS = 32
 
-class Xspress3(Device):
-    """very simple XSPRESS3 interface"""
+class ADFileMixin(object):
+    """mixin class for Xspress3"""
+    def filePut(self, attr, value, **kw):
+        return self.put("%s%s" % (self.filesaver, attr), value, **kw)
+
+    def setFilePath(self, pathname):
+        fullpath = os.path.join(self.fileroot, pathname)
+        return self.filePut('FilePath', fullpath)
+
+    def setFileTemplate(self,fmt):
+        return self.filePut('FileTemplate', fmt)
+
+    def setFileWriteMode(self,mode):
+        return self.filePut('FileWriteMode', mode)
+
+    def setFileName(self,fname):
+        return self.filePut('FileName', fname)
+
+    def nextFileNumber(self):
+        self.setFileNumber(1+self.fileGet('FileNumber'))
+
+    def setFileNumber(self, fnum=None):
+        if fnum is None:
+            self.filePut('AutoIncrement', 1)
+        else:
+            self.filePut('AutoIncrement', 0)
+            return self.filePut('FileNumber',fnum)
+
+    def getLastFileName(self):
+        return self.fileGet('FullFileName_RBV',as_string=True)
+
+    def FileCaptureOn(self):
+        return self.filePut('Capture', 1)
+
+    def FileCaptureOff(self):
+        return self.filePut('Capture', 0)
+
+    def setFileNumCapture(self,n):
+        return self.filePut('NumCapture', n)
+
+    def FileWriteComplete(self):
+        return (0==self.fileGet('WriteFile_RBV') )
+
+    def getFileTemplate(self):
+        return self.fileGet('FileTemplate_RBV',as_string=True)
+
+    def getFileName(self):
+        return self.fileGet('FileName_RBV',as_string=True)
+
+    def getFileNumber(self):
+        return self.fileGet('FileNumber_RBV')
+
+    def getFilePath(self):
+        return self.fileGet('FilePath_RBV',as_string=True)
+
+    def getFileNameByIndex(self,index):
+        return self.getFileTemplate() % (self.getFilePath(), self.getFileName(), index)
+
+class Xspress3BaseMixin(object):
+    """xspress3 mixin -- triggers, acquire, etc"""
+    def useExternalTrigger(self):
+        self.TriggerMode = 3
+
+    def useInternalTrigger(self):
+        self.TriggerMode = 1
+
+    def setTriggerMode(self, mode):
+        self.TriggerMode = mode
+
+    def start(self, capture=True):
+        time.sleep(.05)
+        if capture:
+            self.FileCaptureOn()
+        self.Acquire = 1
+
+    def stop(self):
+        self.Acquire = 0
+        self.FileCaptureOff()
+
+    def get_rois(self):
+        return [m.get_rois() for m in self.mcas]
+
+class Xspress3(Device, ADFileMixin, Xspress3BaseMixin):
+    """Epics Xspress3.20 interface (with areaDetector2)"""
+    det_attrs = ('NumImages', 'NumImages_RBV',
+                 'Acquire', 'Acquire_RBV',
+                 'ArrayCounter_RBV',
+                 'ERASE', 'UPDATE', 'AcquireTime',
+                 'TriggerMode', 'StatusMessage_RBV',
+                 'DetectorState_RBV')
+
+    _nonpvs  = ('_prefix', '_pvs', '_delim', 'filesaver',
+                'fileroot', 'pathattrs', '_nonpvs', '_save_rois',
+                'nmca', 'dxps', 'mcas')
+
+    pathattrs = ('FilePath', 'FileTemplate',
+                 'FileName', 'FileNumber',
+                 'Capture',  'NumCapture')
+
+    def __init__(self, prefix, nmca=4, filesaver='HDF1:',
+                 fileroot='/home/xspress3/cars5/Data'):
+        if not prefix.endswith(':'):
+            prefix = "%s:" % prefix
+        self.nmca = nmca
+        attrs = list(self.attrs)
+        attrs.extend(['%s%s' % (filesaver,p) for p in self.pathattrs])
+
+        self.filesaver = filesaver
+        self.fileroot = fileroot
+        self._prefix = prefix
+        self._save_rois = []
+        for i in range(nmca):
+            imca = i+1
+            dprefix = "%sdet1" % prefix
+            rprefix = "%sMCA%iROI" % (prefix, imca)
+            data_pv = "%sMCA%i:ArrayData" % (prefix, imca)
+            mca = ADMCA(dprefix, mca=imca, data_pv=data_pv, roi_prefix=rprefix)
+            self.mcas.append(mca)
+
+        Device.__init__(self, prefix, attrs=attrs, delim='')
+        for attr in det_attrs:
+            self.add_pv("%sdet1:%s" % (prefix,attr), attr)
+        time.sleep(0.1)
+
+    def roi_calib_info(self):
+        buff = ['[rois]']
+        add = buff.append
+        rois = self.mcas[0].get_rois()
+        for iroi, roi in enumerate(rois):
+            name = roi.Name
+            hi   = roi.MinX + roi.SizeX
+            if len(name.strip()) > 0 and hi > 0:
+                dbuff = []
+                for m in range(self.nmca):
+                    dbuff.extend([roi.MinX, roi.MinX+roi.SizeX])
+                dbuff = ' '.join([str(i) for i in dbuff])
+                add("ROI%2.2i = %s | %s" % (iroi, name, dbuff))
+
+        add('[calibration]')
+        add("OFFSET = %s " % (' '.join(["0.000 "] * self.nmca)))
+        add("SLOPE  = %s " % (' '.join(["0.010 "] * self.nmca)))
+        add("QUAD   = %s " % (' '.join(["0.000 "] * self.nmca)))
+        add('[dxp]')
+        return buff
+
+    def restore_rois(self, roifile):
+        """restore ROI setting from ROI.dat file"""
+        cp =  ConfigParser()
+        cp.read(roifile)
+        rois = []
+        prefix = self.roi_prefix
+        iroi = 0
+        for a in cp.options('rois'):
+            if a.lower().startswith('roi'):
+                name, dat = cp.get('rois', a).split('|')
+                lims = [int(i) for i in dat.split()]
+                lo, hi = lims[0], lims[1]
+                roi = ADMCAROI(prefix=prefix, roi=iroi)
+                roi.MinX= lo
+                roi.SizeX = hi-lo
+                roi.Name = name.strip()
+                rois.append(roi)
+                iroi += 1
+
+        for mca in self.mcas:
+            mca.set_rois(rois)
+
+class Xspress310(Device, ADFileMixin, Xspress3BaseMixin):
+    """Epics Xspress3.10 interface (older version)"""
     attrs = ('NumImages', 'NumImages_RBV',
-             'Acquire', 'Acquire_RBV', 
-             'ArrayCounter_RBV', 
+             'Acquire', 'Acquire_RBV',
+             'ArrayCounter_RBV',
              'ERASE', 'UPDATE', 'AcquireTime',
              'TriggerMode', 'StatusMessage_RBV',
              'DetectorState_RBV')
 
-    _nonpvs  = ('_prefix', '_pvs', '_delim', 'filesaver', 
+    _nonpvs  = ('_prefix', '_pvs', '_delim', 'filesaver',
                 'fileroot', 'pathattrs', '_nonpvs', '_save_rois',
                 'nmca', 'dxps', 'mcas')
 
@@ -41,8 +209,6 @@ class Xspress3(Device):
         Device.__init__(self, prefix, attrs=attrs, delim='')
         time.sleep(0.1)
 
-    def get_rois(self):
-        return [m.get_rois() for m in self.mcas]
 
     def select_rois_to_save(self, roilist):
         """copy rois from MCA record to arrays to be saved
@@ -121,79 +287,3 @@ class Xspress3(Device):
         cal0 = self.mcas[0].get_calib()
         for mca in self.mcas[1:]:
             mca.set_rois(rois, calib=cal0)
-
-    def useExternalTrigger(self):
-        self.TriggerMode = 3
-
-    def useInternalTrigger(self):
-        self.TriggerMode = 1
-
-    def setTriggerMode(self, mode):
-        self.TriggerMode = mode
-
-    def start(self, capture=True):
-        self.ERASE = 1
-        time.sleep(.05)
-        if capture:
-            self.FileCaptureOn()
-        self.Acquire = 1
-
-    def stop(self):
-        self.Acquire = 0
-        self.FileCaptureOff()
-
-    def filePut(self, attr, value, **kw):
-        return self.put("%s%s" % (self.filesaver, attr), value, **kw)
-
-    def setFilePath(self, pathname):
-        fullpath = os.path.join(self.fileroot, pathname)
-        return self.filePut('FilePath', fullpath)
-
-    def setFileTemplate(self,fmt):
-        return self.filePut('FileTemplate', fmt)
-
-    def setFileWriteMode(self,mode):
-        return self.filePut('FileWriteMode', mode)
-
-    def setFileName(self,fname):
-        return self.filePut('FileName', fname)
-
-    def nextFileNumber(self):
-        self.setFileNumber(1+self.fileGet('FileNumber'))
-
-    def setFileNumber(self, fnum=None):
-        if fnum is None:
-            self.filePut('AutoIncrement', 1)
-        else:
-            self.filePut('AutoIncrement', 0)
-            return self.filePut('FileNumber',fnum)
-
-    def getLastFileName(self):
-        return self.fileGet('FullFileName_RBV',as_string=True)
-
-    def FileCaptureOn(self):
-        return self.filePut('Capture', 1)
-
-    def FileCaptureOff(self):
-        return self.filePut('Capture', 0)
-
-    def setFileNumCapture(self,n):
-        return self.filePut('NumCapture', n)
-
-    def FileWriteComplete(self):
-        return (0==self.fileGet('WriteFile_RBV') )
-
-    def getFileTemplate(self):
-        return self.fileGet('FileTemplate_RBV',as_string=True)
-
-    def getFileName(self):
-        return self.fileGet('FileName_RBV',as_string=True)
-
-    def getFileNumber(self):
-        return self.fileGet('FileNumber_RBV')
-
-    def getFilePath(self):
-        return self.fileGet('FilePath_RBV',as_string=True)
-
-    def getFileNameByIndex(self,index):
-        return self.getFileTemplate() % (self.getFilePath(), self.getFileName(), index)
