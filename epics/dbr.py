00eafc697c106b96363b291bbcebd6af3b817e4d
Daron Chabot <chabot@frib.msu.edu>
2017-04-04 13:35:01 -0400

    ENH: access rights handler args

--- epics/dbr.py
+++ epics/dbr.py
@@ -338,6 +338,12 @@ class connection_args(ctypes.Structure):
     _fields_ = [('chid', chid_t), 
                 ('op', long_t)]
 
+class access_rights_handler_args(ctypes.Structure):
+    "access rights arguments"
+    _fields_ = [('chid', chid_t),
+                ('read_access', uint_t, 1),
+                ('write_access', uint_t, 1)]
+
 if PY64_WINDOWS and PY_MAJOR == 2:
     # need to add padding on 64-bit Windows for Python2 -- yuck!
     class event_handler_args(ctypes.Structure):
