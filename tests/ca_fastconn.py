24b41f7ad8882dfb1fa1f9bb308aeea5cb6838e5
Matt Newville <newville@cars.uchicago.edu>
2017-08-31 12:20:19 -0500

    fix line ending and use str.strip() for fastconn_pvlist.txt

--- tests/ca_fastconn.py
+++ tests/ca_fastconn.py
@@ -6,23 +6,26 @@ try:
 except:
     from ordereddict import OrderedDict
 dt = debugtime.debugtime()
-dt.add('test of fast connection to many PVs')
+def add(x):
+    print(x)
+    dt.add(x)
+
+add('test of fast connection to many PVs')
 pvnames = []
 
 results = OrderedDict()
 
 MAX_PVS = 12500
 
-
 for line  in open('fastconn_pvlist.txt','r').readlines():
     if not line.startswith('#'):
-        pvnames.append(line[:-1])
+        pvnames.append(line.strip())
 
 if MAX_PVS is not None:
     pvnames = pvnames[:MAX_PVS]
 
 
-dt.add('Read PV list:  Will connect to %i PVs' % len(pvnames))
+add('Read PV list:  Will connect to %i PVs' % len(pvnames))
 libca = ca.initialize_libca()
 
 for name in pvnames:
@@ -31,14 +34,14 @@ for name in pvnames:
 
 time.sleep(0.001)
 
-dt.add("created PVs with ca_create_channel")
+add("created PVs with ca_create_channel")
 
 for name in pvnames:
     ca.connect_channel(results[name]['chid'])
 
 time.sleep(0.001)
 
-dt.add("connected to PVs with connect_channel")
+add("connected to PVs with connect_channel")
 
 ca.pend_event(1.e-2)
 
@@ -47,20 +50,20 @@ for name in pvnames:
     val = ca.get(chid, wait=False)
     results[name]['value'] =  val
 
-dt.add("did ca.get(wait=False)")
+add("did ca.get(wait=False)")
 ca.poll(2.e-3, 1.0)
-dt.add("ca.poll() complete")
+add("ca.poll() complete")
 
 for name in pvnames:
     results[name]['value'] = ca.get_complete(results[name]['chid'])
 
-dt.add("ca.get_complete() for all PVs")
- 
+add("ca.get_complete() for all PVs")
+
 f = open('fastconn_pvdata.sav', 'w')
 for name, val in results.items():
     f.write("%s %s\n" % (name.strip(), val['value']))
 f.close()
-dt.add("wrote PV values to disk")
+add("wrote PV values to disk")
 
 dt.show()
 
