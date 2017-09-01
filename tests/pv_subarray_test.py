40bf8a0d951a8ec213c26d34379e26ad1073261b
Matt Newville <newville@cars.uchicago.edu>
2017-09-01 11:26:54 -0500

    subarray test needs to use integers for slices

--- tests/pv_subarray_test.py
+++ tests/pv_subarray_test.py
@@ -11,16 +11,15 @@ sub3   = epics.PV(pvnames.subarr3)
 sub4   = epics.PV(pvnames.subarr4)
 
 
-s1_0 =  epics.caget("%s.INDX" % pvnames.subarr1)
-s2_0 =  epics.caget("%s.INDX" % pvnames.subarr2)
-s3_0 =  epics.caget("%s.INDX" % pvnames.subarr3)
-s4_0 =  epics.caget("%s.INDX" % pvnames.subarr4)
+s1_0 =  int(epics.caget("%s.INDX" % pvnames.subarr1))
+s2_0 =  int(epics.caget("%s.INDX" % pvnames.subarr2))
+s3_0 =  int(epics.caget("%s.INDX" % pvnames.subarr3))
+s4_0 =  int(epics.caget("%s.INDX" % pvnames.subarr4))
 
-s1_n =  epics.caget("%s.NELM" % pvnames.subarr1)
-s2_n =  epics.caget("%s.NELM" % pvnames.subarr2)
-s3_n =  epics.caget("%s.NELM" % pvnames.subarr3)
-s4_n =  epics.caget("%s.NELM" % pvnames.subarr4)
-
+s1_n =  int(epics.caget("%s.NELM" % pvnames.subarr1))
+s2_n =  int(epics.caget("%s.NELM" % pvnames.subarr2))
+s3_n =  int(epics.caget("%s.NELM" % pvnames.subarr3))
+s4_n =  int(epics.caget("%s.NELM" % pvnames.subarr4))
 
 npts = len(driver.get())
 
@@ -30,7 +29,6 @@ for i in range(10):
 
     full = driver.get()
     sys.stdout.write("%s\n" % full)
-    
     all_ok = all( [all( sub1.get() == full[s1_0:s1_n+s1_0]), 
                   all( sub2.get() == full[s2_0:s2_n+s2_0]),
                   all( sub3.get() == full[s3_0:s3_n+s3_0]),
