673ddb6039e4a2684e50b658c32b8b8994debc3c
Thomas A Caswell <tcaswell@bnl.gov>
2019-05-30 16:29:43 -0400

    TST: simplify install tests

--- tests/test_install.py
+++ tests/test_install.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-import sys 
+import sys
 import re
 
 import epics
@@ -12,14 +12,14 @@ pytestmark = pytest.mark.skipif(os.getenv('TRAVIS') is None,
 
 def test_libca_init():
     assert epics.ca.initialize_libca()
-    
+
 # Skip if Travis defines NOLIBCA
 @pytest.mark.skipif(os.getenv('NOLIBCA') is not None,
                     reason='NOLIBCA is defined')
 def test_install_encapsulation():
     libca = epics.ca.find_libca()
     system = sys.platform
-    
+
     if 'linux' in system:
         assert re.search('.*/epics/clibs/.*/libca\.so$', libca)
     elif 'darwin' in system:
@@ -33,14 +33,5 @@ def test_install_encapsulation():
 @pytest.mark.skipif(os.getenv('NOLIBCA') is None,
                     reason='NOLIBCA is not defined')
 def test_install_no_libs():
-    libca = epics.ca.find_libca()
-    system = sys.platform
-
-    if 'linux' in system:
-        assert re.search('.*/epics/clibs/.*/libca\.so$', libca) is None
-    elif 'darwin' in system:
-        assert re.search('.*/epics/clibs/.*/libca\.dylib$', libca) is None
-    elif 'win' in system:
-        assert re.search('.*\epics\clibs\.*/ca\\.dll$', libca) is None
-    else:
-        raise 'Are you using BSD? You are hardcore...'
+    with pytest.raises(ImportError):
+        import epics.clibs
