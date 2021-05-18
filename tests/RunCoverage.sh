e8eb72ae5b41c9d67f9222870964b0d7db7e1d36
Matt Newville <newville@cars.uchicago.edu>
2021-05-18 15:32:28 -0500

    update RuncCoverage and README

--- tests/RunCoverage.sh
+++ tests/RunCoverage.sh
@@ -1,29 +1,18 @@
 coverage erase
 
-coverage run --source=epics -a -p --timid  ca_unittest.py
-coverage run --source=epics -a -p --timid  ca_subscribe.py
-coverage run --source=epics -a -p --timid  ca_subscribe2.py
-coverage run --source=epics -a -p --timid  ca_type_conversion.py
-
-coverage run --source=epics -a -p --timid  pv_unittest.py
-coverage run --source=epics -a -p --timid  pv_type_conversion.py
-coverage run --source=epics -a -p --timid  pv_callback.py
-coverage run --source=epics -a -p --timid  pv_connection_callback.py
-
-coverage run --source=epics -a -p --timid  alarm.py
-
-coverage run --source=epics -a -p --timid  ca_connection_callback.py
-coverage run --source=epics -a -p --timid  ca_simpletest.py
-
-
-coverage run --source=epics -a -p --timid  connect.py
-coverage run --source=epics -a -p --timid  memleak.py
-coverage run --source=epics -a -p --timid  no_monitor.py
-coverage run --source=epics -a -p --timid  putwait.py
-coverage run --source=epics -a -p --timid  pv_multiple_callbacks.py
-coverage run --source=epics -a -p --timid  pv_simpletest.py
-coverage run --source=epics -a -p --timid  thread_test.py
+coverage run --source=epics -a -p --timid  test_camonitor_func.py
+coverage run --source=epics -a -p --timid  test_ca_subscribe.py
+coverage run --source=epics -a -p --timid  test_cathread.py
+coverage run --source=epics -a -p --timid  test_ca_typeconversion.py
+coverage run --source=epics -a -p --timid  test_ca_unittests.py
+coverage run --source=epics -a -p --timid  test_multiprocessing.py
+coverage run --source=epics -a -p --timid  test_aodevice.py
+coverage run --source=epics -a -p --timid  test_pv_callback.py
+coverage run --source=epics -a -p --timid  test_pv_initcallbacks.py
+coverage run --source=epics -a -p --timid  test_pvsubarray.py
+coverage run --source=epics -a -p --timid  test_pv_typeconversion.py
+coverage run --source=epics -a -p --timid  test_pv_unittests.py
+coverage run --source=epics -a -p --timid  test_threading.py
 
 coverage combine
-
 coverage report -m
