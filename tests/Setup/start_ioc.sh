37a532ed8432c5b76ae12390c9153073f24c30a6
Matt Newville <newville@cars.uchicago.edu>
2021-05-20 11:58:22 -0500

    better start_ioc script for non-CI uses

--- tests/Setup/start_ioc.sh
+++ tests/Setup/start_ioc.sh
@@ -1,4 +1,22 @@
-$CONDA/bin/procServ -n pyepics_testioc \
-		   --allow --noautorestart \
-		   -P 9230 -L pyepics_testioc.log \
-		   -e $CONDA/epics/bin/linux-x86_64/softIoc ./st.cmd
+#!/bin/sh
+
+SOFTIOC=softIoc
+if ! command -v $SOFTIOC &> /dev/null ; then
+    if [[ -z "${CONDA}" ]]; then
+	echo 'Warning -- cannot find softIoc'
+    else
+	SOFTIOC=$CONDA/epics/bin/linux-x86_64/softIoc
+    fi
+fi
+
+PROCSERV=procServ
+if ! command -v $PROCSERV &> /dev/null ; then
+    if [[ -z "${CONDA}" ]]; then
+	echo 'Warning -- cannot find procServ'
+    else
+	SOFTIOC=$CONDA/bin/procServ
+    fi
+fi
+
+OPTS='-P 9230 -n pyepics_testioc -L pyepics_testioc.log --noautorestart'
+$PROCSERV $OPTS -e $SOFTIOC ./st.cmd
