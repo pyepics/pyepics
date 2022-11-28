5298697cd77b9a8ab06a8b9d5a560e16b187174a
Matt Newville <newville@cars.uchicago.edu>
2022-11-27 22:08:32 -0600

    Enable testing with darwin using screen (#243)
    * enable ioc running with /usr/bin/screen on Darwin
    
    * fixes

--- tests/Setup/start_ioc.sh
+++ tests/Setup/start_ioc.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-SOFTIOC=softIoc
-if ! command -v $SOFTIOC &> /dev/null ; then
-    if [[ -z "${CONDA}" ]]; then
-	echo 'Warning -- cannot find softIoc'
-    else
-	SOFTIOC=$CONDA/epics/bin/linux-x86_64/softIoc
-    fi
-fi
+export EPICS_CA_ADDR_LIST=localhost
+export EPICS_CA_AUTO_ADDR_LIST=NO
+export EPICS_CA_MAX_ARRAY_BYTES=20100300
 
-PROCSERV=procServ
-if ! command -v $PROCSERV &> /dev/null ; then
-    if [[ -z "${CONDA}" ]]; then
-	echo 'Warning -- cannot find procServ'
-    else
-	SOFTIOC=$CONDA/bin/procServ
-    fi
-fi
+PROCSERV=$CONDA/bin/procServ
+PROCSERV_OPTS='-P 9230 -n pyepics_testioc -L pyepics_testioc.log --noautorestart'
 
 if ! test -f ./st.cmd; then
     echo 'Error -- st.cmd not found at the current directory'
@@ -24,5 +13,12 @@ if ! test -f ./st.cmd; then
     exit 1
 fi
 
-OPTS='-P 9230 -n pyepics_testioc -L pyepics_testioc.log --noautorestart'
-$PROCSERV $OPTS -e $SOFTIOC ./st.cmd
+uname=`uname`
+if [ $uname == Darwin ]; then
+    SOFTIOC=$CONDA/epics/bin/darwin-x86/softIoc
+    /usr/bin/screen -d -m $SOFTIOC ./st.cmd
+else
+    SOFTIOC=$CONDA/epics/bin/linux-x86_64/softIoc
+    echo 'Running ioc with ' $PROCSERV $PROCSERV_OPTS -e $SOFTIOC ./st.cmd
+    $PROCSERV $PROCSERV_OPTS -e $SOFTIOC ./st.cmd
+fi
