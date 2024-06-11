710baa15a014561851950cbc61895a0481cdfef8
Matthew Newville <newville@cars.uchicago.edu>
2024-06-11 09:50:49 -0500

    download and build procServ in test suite

--- tests/Setup/start_ioc.sh
+++ tests/Setup/start_ioc.sh
@@ -11,6 +11,9 @@ export EPICS_CA_AUTO_ADDR_LIST=NO
 export EPICS_CA_MAX_ARRAY_BYTES=20100300
 export EPICS_HOST_ARCH=linux-x86_64
 
+PROCSERV=$CONDA/bin/procServ
+PROCSERV_OPTS='-P 9230 -n pyepics_testioc -L pyepics_testioc.log --noautorestart'
+
 uname=`uname`
 if [ $uname == Darwin ]; then
     export EPICS_HOST_ARCH=darwin-x86
@@ -18,4 +21,4 @@ fi
 
 export PATH=$CONDA/epics/bin/$EPICS_HOST_ARCH/softIoc:$PATH
 
-softIoc ./st.cmd
+$PROCSERV $PROCSERV_OPTS softIoc ./st.cmd
