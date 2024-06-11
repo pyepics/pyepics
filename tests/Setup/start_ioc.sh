c0b181d7a2e097503bc1e46d51a601174cf1bd2d
Matthew Newville <newville@cars.uchicago.edu>
2024-06-11 09:15:22 -0500

    not relying on procServ to run test_ioc for test suite

--- tests/Setup/start_ioc.sh
+++ tests/Setup/start_ioc.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-export EPICS_CA_ADDR_LIST=localhost
-export EPICS_CA_AUTO_ADDR_LIST=NO
-export EPICS_CA_MAX_ARRAY_BYTES=20100300
-
-PROCSERV=$CONDA/bin/procServ
-PROCSERV_OPTS='-P 9230 -n pyepics_testioc -L pyepics_testioc.log --noautorestart'
-
 if ! test -f ./st.cmd; then
     echo 'Error -- st.cmd not found at the current directory'
     echo 'Run the script with pwd at <repo>/tests/Setup/st.cmd'
     exit 1
 fi
 
+export EPICS_CA_ADDR_LIST=localhost
+export EPICS_CA_AUTO_ADDR_LIST=NO
+export EPICS_CA_MAX_ARRAY_BYTES=20100300
+export EPICS_HOST_ARCH=linux-x86_64
+
 uname=`uname`
 if [ $uname == Darwin ]; then
-    SOFTIOC=$CONDA/epics/bin/darwin-x86/softIoc
-    /usr/bin/screen -d -m $SOFTIOC ./st.cmd
-else
-    SOFTIOC=$CONDA/epics/bin/linux-x86_64/softIoc
-    echo 'Running ioc with ' $PROCSERV $PROCSERV_OPTS -e $SOFTIOC ./st.cmd
-    $PROCSERV $PROCSERV_OPTS -e $SOFTIOC ./st.cmd
+    export EPICS_HOST_ARCH=darwin-x86
 fi
+
+export PATH=$CONDA/epics/bin/$EPICS_HOST_ARCH/softIoc:$PATH
+
+softIoc ./st.cmd
