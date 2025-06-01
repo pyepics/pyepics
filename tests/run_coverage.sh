7d0f746df564cb8fd1e91cf7d855a13f2e5469ce
Matthew Newville <newville@cars.uchicago.edu>
2025-06-01 13:08:11 -0500

    add disconnect test to coverage run, alphabetical order

--- tests/run_coverage.sh
+++ tests/run_coverage.sh
@@ -1,17 +1,18 @@
 coverage erase
-pytest test_camonitor_func.py
-pytest test_ca_subscribe.py
+pytest test_aodevice.py
 pytest test_ca_clearcache.py
-pytest test_cathread.py
+pytest test_ca_subscribe.py
 pytest test_ca_typeconversion.py
 pytest test_ca_unittests.py
+pytest test_camonitor_func.py
+pytest test_cathread.py
 pytest test_multiprocessing.py
-pytest test_aodevice.py
 pytest test_pv_callback.py
+pytest test_pv_disconnect.py
 pytest test_pv_initcallbacks.py
-pytest test_pvsubarray.py
 pytest test_pv_typeconversion.py
 pytest test_pv_unittests.py
+pytest test_pvsubarray.py
 pytest test_threading.py
 coverage combine
 coverage report -m --omit "wxlib*,ogl*,motor*,mca*,ad*,struck*,transform*,scan*,scal*,xspress*"
